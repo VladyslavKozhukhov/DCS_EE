5a080d369a1c7170e0007bfb1207f72376774f04
portnoybar <55344891+portnoybar@users.noreply.github.com>
2020-05-29 12:17:01 +0300

    q2 update

--- Lab2/main.c
+++ Lab2/main.c
@@ -5,10 +5,11 @@ int flag = 0;
 int R5  = 0;
 int R4 = 0;
 void main(void)
+
+unsigned int ADCResult = 0;
+
 {
   WDTCTL = WDTPW+WDTHOLD;                   // Stop WDT
-  
- 
   
   // Configure PBs
   P1DIR &= ~(BIT4+BIT5+BIT6);          		// P1.4-P1.6 input
@@ -26,10 +27,19 @@ void main(void)
   
   // Configure P3.4 as TB3 (Compare mode)
   P3SEL |= BIT4;                            
-  P3DIR |= BIT4;   
+  P3DIR |= BIT4; 
+  TB0CCTL1 = OUTMOD_7;  
+  TB0CTL = TBSSEL_2 + MC_1;	//TBSSEL_2 selects SMCLK as the clock source, and MC_1 tells it to count up to the value in TB0CCR0.
   
-  // Configure P6.3 as A3
-  P6SEL |= BIT3;
+  
+  // Configure ADC12
+  P6SEL |= 0x08;		// Configure P6.3 as A3
+  ADC12MCTL3 = INCH_3; 	// Analog input is A3, VR+=3.3v VR-=0v
+  ADC12CTL0 = SHT0_2+ADC12ON;
+  // 
+  ADC12CTL1 = SHP+CSTARTADD_3+ADCSSEL1;	// Choose ACLK
+  ADC12CTL0 |= ENC;
+  
   
   // Config capture on rising clock +MLCK+triger enable 
 	TBCCTL2|=CM_1+SCS+CAP+CCIE;
@@ -52,6 +62,9 @@ void main(void)
 
 __interrupt void Port1_ISR (void)
 {
+	
+	// Not sure!
+  ADC12CTL0 &= ~ADC12SC;					// Disable ADC12 sampling/conversion
   
 //---------------------------------------------------------------
 //                  PB0 - Frequancy counter
@@ -76,12 +89,25 @@ __interrupt void Port1_ISR (void)
 //---------------------------------------------------------------
 //                  PB1 - Derivative detector
 //--------------------------------------------------------------- 
-  
+  // Recommended sampling frequency is 2*max(fin) = 100Hz according to Nyquist.
+  /* TODO:
+	 if we choose aclk, divide by 8 => cycle = (1/4096) sec
+
+	conversion  = 13 + n + tsync 
+	SHP = 1
+	SHT = 1001 = 384 = n
+
+	conversion time = (13 + 384 + tsync)*(1/4096) = ~0.097 sec => ~100Hz
+	
+  */
   else if (P1IFG & BIT5) {                    //P1.5 interruped
-	/*
+  
+	ADC12IE |= 0x0003;		// Enable ADC12 interrupt A3
+	ADC12CTL0 |= ADC12SC; 	// Start sampling/conversion
 	
+	// Not sure!
+	_BIS_SR(CPUOFF+GIE);	// LPM0, ADC12_ISR will force exit
 	
-	*/
     P1IFG &= ~BIT5;                           // P1.5 IFG Cleared
   }
   
@@ -94,4 +120,20 @@ __interrupt void Port1_ISR (void)
     P1IFG &= ~BIT6;                           	// P1.6 IFG Cleared
   }
   
+}
+
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
+{
+  if (ADC12MEM3 > ADCResult){               	// current sample is grater then before v'in > 1 => P3.4 = 1KHz
+    TB0CCR0 = 4000;                          // Set the period in the Timer B0 Capture/Compare 0 register to 4000 us = 4KHz.
+	TB0CCR3 = 2000;							//The period in microseconds that the power is ON. It translates to a 50% duty cycle.
+  }
+  else (ADC12MEM3 < ADCResult){				// current sample is smaller then before v'in < 1 => P3.4 = 4KHz
+    TB0CCR0 = 1000;                          // Set the period in the Timer B0 Capture/Compare 0 register to 1000 us = 1KHz.
+	TB0CCR3 = 500;							//The period in microseconds that the power is ON. It translates to a 50% duty cycle.
+  }
+	else									// current sample is equal to before v'in = 0 => '0' => P3.4 = 0
+		P3OUT &=~BIT4;
+	ADCResult = ADC12MEM3;					// Store current sample
 }
