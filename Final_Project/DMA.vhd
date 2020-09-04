9b5387a3978a50fcb901e94f7bff1d0413df20e6
bar <portnoybar@gmail.com>
2020-09-04 11:14:00 +0300

    update DMA

--- Final_Project/DMA.vhd
+++ Final_Project/DMA.vhd
@@ -1,69 +1,82 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use ieee.numeric_std.all;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.ALL;
 
 ENTITY DMA IS
-  GENERIC (
-    size: integer:=16;
-    address_size: integer:=7
-    );
-  PORT (	
-		clk:		    IN std_logic;
-		rst:      IN std_logic;
-		en:       IN std_logic;
-		DREQ:		   IN std_logic;
-		HOLDA:    IN std_logic;
-		HOLD:     OUT std_logic;
-		DACK:     OUT std_logic;		
-		address:  OUT std_logic_vector(address_size-1 downto 0);
-		RW:       OUT std_logic;
-		EOP:      OUT std_logic
-		);
-end DMA;
+	GENERIC (
+		size : INTEGER := 16;
+		address_size : INTEGER := 7
+	);
+	PORT (
+		clk : IN std_logic;
+		rst : IN std_logic;
+		en : IN std_logic;
+		DREQ0 : IN std_logic;
+		DREQ1 : IN std_logic;
+		HOLDA : IN std_logic;
+		HOLD : OUT std_logic;
+		DACK0 : OUT std_logic; 
+		DACK1 : OUT std_logic; 
+		address : OUT std_logic_vector(address_size - 1 DOWNTO 0);
+		RW : OUT std_logic;
+		ALE : OUT std_logic;
+		EOP : OUT std_logic
+	);
+END DMA;
 
-architecture DMA_behavioral of DMA is
-  
-  begin
-    process(clk) 
-      variable transfers_count: integer:=0;
-      constant MAX_DMA_REQUESTS: integer:=99;
-      begin
-        if (rst = '1') then
-		      HOLD <= '0';
-          DACK <= '0';
-          address <= (others => 'Z'); -- disconnect from bus
-          RW <= '0';
-      		  transfers_count := 0; -- reset counter
-		    else
-		      if (en = '1') then
-            if (rising_edge(clk)) then
-              if (DREQ = '1' and HOLDA = '0') then
-                HOLD <= '1';  -- send HOLD to CPU
-                DACK <= '0';
-                address <= (others => 'Z'); -- disconnect from bus
-                RW <= '0';
-              elsif (DREQ = '1' and HOLDA = '1') then
-                HOLD <= '0';
-                DACK <= '1';  -- send DACK to Timer
-                address <=  std_logic_vector(to_unsigned(transfers_count, address'length)); -- put MEM address
-                RW <= '1';
-                transfers_count := transfers_count+1;
-                -- EOP
-                if (transfers_count = MAX_DMA_REQUESTS) then
-                  transfers_count := 0;
-                  EOP <= '1';
-                else
-                  EOP <= '0';
-                end if;
-              end if;
-            end if;
-          else  -- if idle
-            HOLD <= '0';
-            DACK <= '0';
-            address <= (others => 'Z'); -- disconnect from bus
-            RW <= '0';
-          end if;
-      end if;
-    end process;
-    
-end DMA_behavioral;
+ARCHITECTURE DMA_behavioral OF DMA IS
+ 
+BEGIN
+	PROCESS (clk)
+	VARIABLE transfers_count : INTEGER := 0;
+	CONSTANT MAX_DMA_REQUESTS : INTEGER := 99;
+	BEGIN
+		IF (rst = '1') THEN
+			HOLD <= '0';
+			DACK0 <= '0';
+			DACK1 <= '0';
+			address <= (OTHERS => 'Z'); -- disconnect from bus
+			RW <= '0';
+			transfers_count := 0; -- reset counter
+		ELSE
+			IF (en = '1') THEN
+				IF (rising_edge(clk)) THEN
+					IF ((DREQ0 = '1' OR DREQ1 = '1') AND HOLDA = '0') THEN
+						HOLD <= '1'; -- send HOLD to CPU
+						IF (DREQ0 = '1') THEN
+							DACK0 <= '0';
+						ELSE -- DREQ1 == '1'
+							DACK1 <= '0';
+						END IF;
+						address <= (OTHERS => 'Z'); -- disconnect from bus
+						RW <= '0';
+					ELSIF ((DREQ0 = '1' OR DREQ1 = '1') AND HOLDA = '1') THEN
+						HOLD <= '0';
+						IF (DREQ0 = '1') THEN
+							DACK0 <= '1';
+						ELSE -- DREQ1 == '1'
+							DACK1 <= '1';
+						END IF;
+						address <= std_logic_vector(to_unsigned(transfers_count, address'length)); -- put MEM address
+						RW <= '1';
+						transfers_count := transfers_count + 1;
+						-- EOP
+						IF (transfers_count = MAX_DMA_REQUESTS) THEN
+							transfers_count := 0;
+							EOP <= '1';
+						ELSE
+							EOP <= '0';
+						END IF;
+					END IF;
+				END IF;
+			ELSE -- if idle
+				HOLD <= '0';
+				DACK0 <= '0';
+				DACK1 <= '0';
+				address <= (OTHERS => 'Z'); -- disconnect from bus
+				RW <= '0';
+			END IF;
+		END IF;
+	END PROCESS;
+ 
+END DMA_behavioral;
--- Final_Project/DMA_tb.vhd
+++ Final_Project/DMA_tb.vhd
@@ -1,80 +1,83 @@
-library ieee;
-use ieee.std_logic_1164.all;
- 
-entity DMA_tb is
-end DMA_tb;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 
-architecture Behavioral of DMA_tb is
+ENTITY DMA_tb IS
+END DMA_tb;
 
-component DMA IS
-  GENERIC (
-    size: integer;
-    address_size: integer
-    );
-  PORT (	
-		clk:		    IN std_logic;
-		rst:      IN std_logic;
-		en:       IN std_logic;
-		DREQ:		   IN std_logic;
-		HOLDA:    IN std_logic;
-		HOLD:     OUT std_logic;
-		DACK:     OUT std_logic;		
-		address:  OUT std_logic_vector(address_size-1 downto 0);
-		RW:       OUT std_logic;
-		EOP:      OUT std_logic
+ARCHITECTURE Behavioral OF DMA_tb IS
+
+	COMPONENT DMA IS
+		GENERIC (
+			size : INTEGER;
+			address_size : INTEGER
 		);
-end component;
+		PORT (
+			clk : IN std_logic;
+			rst : IN std_logic;
+			en : IN std_logic;
+			DREQ0 : IN std_logic;
+			DREQ1 : IN std_logic;
+			HOLDA : IN std_logic;
+			HOLD : OUT std_logic;
+			DACK0 : OUT std_logic;
+			DACK1 : OUT std_logic;
+			address : OUT std_logic_vector(address_size - 1 DOWNTO 0);
+			RW : OUT std_logic;
+			ALE : OUT std_logic;
+			EOP : OUT std_logic
+		);
+	END COMPONENT;
 
-signal size: integer := 16;
-signal address_size: integer := 7;
-signal clk, rst, en, DREQ, HOLDA, HOLD, DACK, RW, EOP: std_logic;
-signal address:std_logic_vector(address_size-1 downto 0);
+	SIGNAL size : INTEGER := 16;
+	SIGNAL address_size : INTEGER := 7;
+	SIGNAL clk, rst, en, DREQ0, DREQ1, HOLDA, HOLD, DACK0, DACK1, RW, EOP, ALE : std_logic;
+	SIGNAL address : std_logic_vector(address_size - 1 DOWNTO 0);
 
-begin
-dut: DMA 
-generic map (size=>size, address_size=>address_size) 
-port map (clk => clk, rst=>rst, en=>en, DREQ=>DREQ, HOLDA=>HOLDA, HOLD=>HOLD, DACK=>DACK, address=>address, RW=>RW, EOP=>EOP);
-  
-   -- Clock process definitions
-clock_process :process
-begin
-     clk <= '0';
-     wait for 25 ns;
-     clk <= '1';
-     wait for 25 ns;
-end process;
+BEGIN
+	dut : DMA
+		GENERIC MAP(size => size, address_size => address_size)
+	PORT MAP(clk => clk, rst => rst, en => en, DREQ0 => DREQ0, DREQ1 => DREQ1, HOLDA => HOLDA, HOLD => HOLD, DACK0 => DACK0, DACK1 => DACK1, address => address, RW => RW, EOP => EOP, ALE => ALE);
+ 
+	-- Clock process definitions
+	clock_process : PROCESS
+	BEGIN
+		clk <= '0';
+		WAIT FOR 25 ns;
+		clk <= '1';
+		WAIT FOR 25 ns;
+	END PROCESS;
 
-gen_HOLDA: process
-begin
-  wait for 50 ns;
-  if (HOLD = '1') then
-    HOLDA <= '1';
-  else
-    HOLDA <= '0';
-  end if;
-end process;
+	gen_HOLDA : PROCESS
+	BEGIN
+		WAIT FOR 50 ns;
+		IF (HOLD = '1') THEN
+			HOLDA <= '1';
+		ELSE
+			HOLDA <= '0';
+		END IF;
+	END PROCESS;
 
-gen_DREQ: process
-begin
-  DREQ <= '1';
-  wait until HOLDA = '1';
-  wait for 50ns;
-  DREQ <= '0';
-  wait until HOLDA = '0';
-  wait for 50ns;
-end process;
+	gen_DREQ : PROCESS
+	BEGIN
+		DREQ0 <= '1';
+		WAIT UNTIL HOLDA = '1';
+		WAIT FOR 50ns;
+		DREQ0 <= '0';
+		WAIT UNTIL HOLDA = '0';
+		WAIT FOR 50ns;
+	END PROCESS;
 
--- Stimulus process
-stim_proc: process
-begin        
-    rst <= '1';
-    en <= '1';
-    wait for 100 ns;    
-    rst <= '0';
-    wait for 500 ns;
-    en <= '0';
-    wait for 100 ns;
-    en <= '1';
-   wait;
-end process;
-end Behavioral;
+	-- Stimulus process
+	stim_proc : PROCESS
+	BEGIN
+		rst <= '1';
+		en <= '1';
+		WAIT FOR 100 ns; 
+		rst <= '0';
+		WAIT FOR 500 ns;
+		en <= '0';
+		WAIT FOR 100 ns;
+		en <= '1';
+		WAIT;
+	END PROCESS;
+END Behavioral;
